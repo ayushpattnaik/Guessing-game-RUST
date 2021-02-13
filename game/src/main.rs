3a38ca36c86064f997ec4df1e5a99d4bc90b5ee2
Ayush Pattnaik <pattnaikayush21@gmail.com>
2021-02-13 11:06:50 +0000

    added cargo

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,11 +1,14 @@
 //Guessing game in rust
 use std::io;
+use rand::Rng;
 fn main() {
   println!("Guess the number!");
   println!("Please input your gress: ");
+  let sec_num=rand::thread_rng().gen_range(1,101);
   let mut guess= String::new();
   io::stdin()
      .read_line(&mut guess)
      .expect("Failed to read line");
-     println!("You guessed the number:\n{}", guess);
+  print!("You guessed the number:{}", guess);
+  println!("The secret number is {}",sec_num);
 }
